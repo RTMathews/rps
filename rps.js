d07e2c57e9abf712b6e14e0065dcf6282b1cfc19
Ryan <mathewsthomasryan@gmail.com>
2023-11-11 11:11:02 -0600

    outcomeDiv

--- rps.js
+++ rps.js
@@ -32,31 +32,37 @@ function playRound(playerSelection, computerSelection)
     {
         const p = document.createElement('p')
         p.innerText = 'You Win! Rock beats Scissors.'
+        outcomeDiv.appendChild('p')
     }
     else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')
     {
         const p = document.createElement('p')
         p.innerText = 'You Win! Scissors beats Paper.'
+        outcomeDiv.appendChild('p')
     }
     else if (playerSelection === 'PAPER' && computerSelection === 'ROCK')
     {
         const p = document.createElement('p')
         p.innerText = 'You Win! Paper beats Rock.'
+        outcomeDiv.appendChild('p')
     }
     else if (computerSelection === 'ROCK' && playerSelection === 'SCISSORS')
     {
         const p = document.createElement('p')
         p.innerText = 'You Lose! Rock beats Scissors.'
+        outcomeDiv.appendChild('p')
     }
     else if (computerSelection === 'SCISSORS' && playerSelection === 'PAPER')
     {
         const p = document.createElement('p')
         p.innerText = 'You Lose! Scissors beats Paper.'
+        outcomeDiv.appendChild('p')
     }
     else if (computerSelection === 'PAPER' && playerSelection === 'ROCK')
     {
         const p = document.createElement('p')
         p.innerText = 'You Lose! Paper beats Rock.'
+        outcomeDiv.appendChild('p')
     }
 }
 
