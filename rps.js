420d8b5fa90adeb2415dd5fb9464ef89de7f2199
Ryan <mathewsthomasryan@gmail.com>
2023-11-11 11:13:11 -0600

    remove quotes appendChild

--- rps.js
+++ rps.js
@@ -26,43 +26,43 @@ function playRound(playerSelection, computerSelection)
     {
         const p = document.createElement('p')
         p.innerText = `Tied! You both picked ${playerSelection}`
-        outcomeDiv.appendChild('p')
+        outcomeDiv.appendChild(p)
     }
     else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS')
     {
         const p = document.createElement('p')
         p.innerText = 'You Win! Rock beats Scissors.'
-        outcomeDiv.appendChild('p')
+        outcomeDiv.appendChild(p)
     }
     else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')
     {
         const p = document.createElement('p')
         p.innerText = 'You Win! Scissors beats Paper.'
-        outcomeDiv.appendChild('p')
+        outcomeDiv.appendChild(p)
     }
     else if (playerSelection === 'PAPER' && computerSelection === 'ROCK')
     {
         const p = document.createElement('p')
         p.innerText = 'You Win! Paper beats Rock.'
-        outcomeDiv.appendChild('p')
+        outcomeDiv.appendChild(p)
     }
     else if (computerSelection === 'ROCK' && playerSelection === 'SCISSORS')
     {
         const p = document.createElement('p')
         p.innerText = 'You Lose! Rock beats Scissors.'
-        outcomeDiv.appendChild('p')
+        outcomeDiv.appendChild(p)
     }
     else if (computerSelection === 'SCISSORS' && playerSelection === 'PAPER')
     {
         const p = document.createElement('p')
         p.innerText = 'You Lose! Scissors beats Paper.'
-        outcomeDiv.appendChild('p')
+        outcomeDiv.appendChild(p)
     }
     else if (computerSelection === 'PAPER' && playerSelection === 'ROCK')
     {
         const p = document.createElement('p')
         p.innerText = 'You Lose! Paper beats Rock.'
-        outcomeDiv.appendChild('p')
+        outcomeDiv.appendChild(p)
     }
 }
 
